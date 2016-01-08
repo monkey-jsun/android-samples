baa3cddd9941ee63d37977d5f4c780af71c4f861
Jun Sun <jsun@junsun.net>
2016-01-07 23:12:04 -0800

    Add activity lifecycle demo (from alex)
    Signed-off-by: Jun Sun <jsun@junsun.net>

---
+++ 04-ActivityLifecycle/lifecycle/src/main/java/edu/scu/c03lifecycle/MainActivity.java
@@ -0,0 +1,73 @@
+package edu.scu.c03lifecycle;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    private int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "in onCreate", Toast.LENGTH_SHORT).show();
+
+        // The SharedPreferences data is private to this app if Context.MODE_PRIVATE is used.
+        final SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        count = sharedPref.getInt("counter", 0);
+
+        final TextView textView = (TextView) findViewById(R.id.counter);
+        textView.setText(Integer.toString(count));
+
+        Button button = (Button) findViewById(R.id.my_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                textView.setText(Integer.toString(count));
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "in onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "in onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "in onPause", Toast.LENGTH_SHORT).show();
+
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("counter", count);
+        editor.commit();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "in onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "in onDestroy", Toast.LENGTH_SHORT).show();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # android-samples
 sample code for Android Programming course in SCU/Sofia, winter of 2016
+
+Works and tested with Android Studio 1.5.1, Android v6.0/API 23, Build Tools v23.0.2
+
+Recommended usage:
+
+download or clone the repository to local file system
+use Android Studio to open each sample project. NOTE: import from github does not work somehow.
